01a416503a54df9bef028c9ea061d759f7cf89c3
Jochen Klar <git@jochenklar.de>
2014-02-06 09:57:55 +0100

    some code

--- lib/daiquiri_admin.py
+++ lib/daiquiri_admin.py
@@ -1,4 +1,17 @@
-
-
 class DaiquiriAdmin():
-    pass
+
+    def __init__(self, baseUrl):
+        self.baseUrl = baseUrl
+        print self.baseUrl
+
+    def get(self, path):
+        pass
+
+    def post(self, path):
+        pass
+
+    def getUser():
+        pass
+
+    def getPassword():
+        pass
