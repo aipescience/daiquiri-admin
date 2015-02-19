1a4d446f3b001df7689b946b22cbbadad02a6e58
Kristin Riebe <kriebe@aip.de>
2015-02-19 14:59:13 +0100

    Return jobId when submitting, added some more useful functions

--- lib/daiquiri_admin.py
+++ lib/daiquiri_admin.py
@@ -1,5 +1,6 @@
 import sys,requests,getpass,urllib2,base64
 from lxml import etree
+import fnmatch
 
 class DaiquiriAdmin():
     def __init__(self, baseUrl, username=None, password=None):
@@ -217,7 +218,7 @@ class DaiquiriAdmin():
         jobs = []
         for node in root.findall(ns + 'jobref'):
             jobs.append({
-                'table': node.attrib['id'],
+                'table': node.attrib['id'],   # This is actually the job's name, not the 'intended' table name in parameter list. Maybe rename to 'name'?
                 'id': node.attrib[xlink + 'href'].split('/')[-1],
                 'status': node.find(ns + 'phase').text
             })
@@ -247,7 +248,7 @@ class DaiquiriAdmin():
 
         for node in root.find(ns + 'parameters').findall(ns + 'parameter'):
             if node.attrib['id'] == 'table':
-                return node.text
+                return jobId, node.text
 
     def deleteUwsJob(self, jobId):
         self.delete('/uws/query/' + jobId, json=False)
@@ -271,7 +272,74 @@ class DaiquiriAdmin():
                 url = node.attrib[xlink + 'href']
                 self.download(url,name + '.' + format)
 
-        # return jobs
+        # return url
+
+
+    def fetchUwsResultUrl(self, jobId, format):
+        """Get url of result for given jobId and format"""
+
+        response = self.get('/uws/query/' + jobId + 'results/', json=False)
+
+        # remove first line
+        string = '\n'.join(response.split('\n')[1:])
+        root = etree.XML(string)
+        ns = '{http://www.ivoa.net/xml/UWS/v1.0}'
+        xlink = '{http://www.w3.org/1999/xlink}'
+
+        for node in root.find(ns + 'results').findall(ns + 'result'):
+
+            if format == node.attrib['id']:
+                url = node.attrib[xlink + 'href']
+
+        return url
+
+    def getUwsJob(self, jobId):
+        """Get details of the job with given jobId"""
+
+        response = self.get('/uws/query/' + jobId , json=False)
+
+        # remove first line
+        string = '\n'.join(response.split('\n')[1:])
+        root = etree.XML(string)
+        ns = '{http://www.ivoa.net/xml/UWS/v1.0}'
+        xlink = '{http://www.w3.org/1999/xlink}'
+
+        job = {}
+        job['id'] = root.find(ns + 'jobId').text
+        job['status'] = root.find(ns + 'phase').text
+
+        if root.find(ns + 'errorSummary') is not None:
+            node = root.find(ns + 'errorSummary').find(ns + 'message')
+            job['errors'] = node.text
+
+        for node in root.find(ns + 'parameters').findall(ns + 'parameter'):
+            if node.attrib['id'] == 'table':
+                job['table'] = node.text
+
+        return job
+
+    def getUwsJobsByNameAndPhase(self, jobname, phase):
+        """Get jobIds matching given job name (pattern) and phase
+        Uses fnmatch, so table name pattern examples are:
+        *result-*, validate-[1-3]*
+        """
+        # NOTE: Usually, job name = result table name, unless there was
+        # an error when submitting the query. This routine *won't* return
+        # all jobs whose *intended* table name matches the given name,
+        # only those which really have this name as their jobname
+        # (id-attribute in jobref) are returned.
+
+        jobs = self.fetchUwsJobs()
+
+        joblist = []
+        for job in jobs:
+            if fnmatch.fnmatch(job['table'], jobname):
+                if fnmatch.fnmatch(job['status'], phase):
+                    joblist.append(job)
+
+        return joblist
+
+
 
 class DaiquiriException(Exception):
     def __init__(self, errors):
