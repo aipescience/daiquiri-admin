d18f424e73e25dfebac314f778c8d8ccd2113a8d
Jochen Klar <jklar@aip.de>
2015-10-09 16:48:35 +0200

    fetch_queries added

--- daiquiri/query.py
+++ daiquiri/query.py
@@ -1,3 +1,17 @@
+from daiquiri.exceptions import DaiquiriException
+
+
 class Query():
     def __init__(self, connection):
         self.connection = connection
+
+    def fetch_queries(self, year, month):
+        response = self.connection.post('/query/jobs/export/', {
+            'year': year,
+            'month': month
+        })
+
+        if response['status'] != 'ok':
+            raise DaiquiriException(response['errors'])
+        else:
+            return response['rows']
